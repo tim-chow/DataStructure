1a201f72bde3b13f5b531194fc9b05e11b4c1a5c
周井江-技术中心-服务研发部 <zhoujingjiang@gomeplus.com>
2017-10-17 09:12:09 +0800

    fix bug

--- 排序算法/希尔排序/shell_sort.py
+++ 排序算法/希尔排序/shell_sort.py
@@ -1,38 +1,32 @@
 class ShellSort:
     @staticmethod
-    def tee(size, batch, start=0):
-        offset = start
-        while offset <= start + size - 1:
-            end = offset + batch - 1
-            if end > start + size - 1:
-                end = start + size - 1
-            yield offset, end
-            offset = end + 1
-
-    @staticmethod
-    def shell_insertion_sort(array, start, end):
-        for i in range(start+1, end+1):
-            for j in range(start, i):
-                if array[i] >= array[j]:
-                    continue
-                sentinel = array[i]
-                # move array[j...i-1] ---> array[j+1...i]
-                for k in range(i, j, -1):
-                    array[k] = array[k-1]
-                array[j] = sentinel
+    def group_sort(array, gap):
+        n = len(array)
+        for i in range(n - gap):
+            j = 1
+            while (i + gap * j) < n:
+                for p in range(j):
+                    if array[i + gap * j] >= array[i + gap * p]:
+                        continue
+                    temp = array[i + gap * j]
+                    for q in range(j, p, -1):
+                        array[i + gap * q] = array[i + gap * (q - 1)]
+                    array[i + gap * p] = temp
+                    break
+                j = j + 1
 
     @classmethod
     def shell_sort(cls, array):
-        for batch in [3, 5, len(array)]:
-            for start, end in cls.tee(len(array), batch, 0):
-                cls.shell_insertion_sort(array, start, end)
-            print batch, ":", array
+        gap = len(array) / 2
+        while gap >= 1:
+            cls.group_sort(array, gap)
+            gap = gap / 2
 
 if __name__ == "__main__":
     import random
     lst = range(20)
     random.shuffle(lst)
-    print "input: ", lst
+    print lst
     ShellSort.shell_sort(lst)
-    print "output: ", lst
+    print lst
 
