5f5dea49f8639e857f0ed3ca1eebf7ea78736857
TimChow <jordan23nbastar@vip.qq.com>
2019-04-16 15:37:14 +0800

    Update BagProblem.py

--- 动态规划/BagProblem.py
+++ 动态规划/BagProblem.py
@@ -1,52 +1,39 @@
-#coding: utf8
+# coding: utf8
 
-class BagProblem:
-    def __init__(self, goods):
-        self._goods = goods
+class BagProblem(object):
+    def __init__(self):
         self._cache = {}
 
-    def dp(self, total_weight):
-        return self._dp(total_weight, 0, tuple())
-
-    def _dp(self, total_weight, sn, chosen):
-        # 如果该子问题已经求解过，则直接从缓存中获取结果，
-        # + 以减少重复计算
-        if (total_weight, sn, chosen) in self._cache:
-            return self._cache[key]
-
-        goods = self._goods
-        # 结束状态是 对最后一个物品作出了决策，或者
-        # + 背包已经装满
-        if sn >= len(goods) or total_weight == 0:
-            return chosen
-
-        # 所有可能是局部解是：
-        # + 1，第sn个物品无法装入背包
-        if goods[sn][0] > total_weight:
-            result = self._dp(total_weight, sn+1, chosen)
-            self._cache[(total_weight, sn, chosen)] = result
+    def cache(self, f):
+        def _inner(sn, current_weight, chosen):
+            key = (sn, current_weight)
+            if key in self._cache:
+                return self._cache[key]
+            result = f(sn, current_weight, chosen)
+            self._cache[key] = result
             return result
+        return _inner
 
-        # + 2，选择第sn个物品
-        total_weight_1 = total_weight - goods[sn][0]
-        chosen_1 = self._dp(total_weight - goods[sn][0], 
-            sn+1, chosen + (goods[sn], ))
-        # + 3，不选择第sn个物品
-        chosen_2 = self._dp(total_weight, sn+1, chosen)
-
-        # 保留可能到达最优解的局部解
-        if sum(good[1] for good in chosen_1) > \
-            sum(good[1] for good in chosen_2):
-            result = chosen_1
-        else:
-            result = chosen_2
-
-        # 将解过的子问题缓存起来
-        self._cache[(total_weight, sn, chosen)] = result
-        return result
+    def calc(self, goods, totoal_weight):
+        @self.cache
+        def _calc(sn, current_weight, chosen):
+            if current_weight == totoal_weight or sn == len(goods) - 1:
+                return current_weight, chosen[:]
+            # 装不进去，则不装
+            if current_weight + goods[sn][0] > totoal_weight:
+                return _calc(sn+1, current_weight, chosen[:])
+            # + 能装进去，分两种情况：
+            # 1，不选择sn
+            totoal_weight1, chosen1 = _calc(sn+1, current_weight, chosen[:])
+            # 2，选择sn
+            totoal_weight2, chosen2 = _calc(sn+1, current_weight+goods[sn][0], chosen+[goods[sn]])
+            if sum([item[1] for item in chosen1]) > sum([item[1] for item in chosen2]):
+                return totoal_weight1, chosen1
+            return totoal_weight2, chosen2
+        return _calc(0, 0, [])
 
 if __name__ == "__main__":
-    goods = [(3, 4), (2, 4), (5, 8), (7, 10)]
-    bag = BagProblem(goods)
-    print bag.dp(11)
-
+    bp = BagProblem()
+    goods = [(3, 4), (2, 4), (5, 8), (7, 10), (3, 3), (8, 11)]
+    totoal_weight = 15
+    print(bp.calc(goods, totoal_weight))
