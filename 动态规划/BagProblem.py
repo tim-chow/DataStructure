b8a35265ca5730ce18e56578b79d91a25c9ce6af
timchow <744475502@qq.com>
2020-06-12 17:46:09 +0800

    refactor

--- 动态规划/BagProblem.py
+++ 动态规划/BagProblem.py
@@ -1,39 +1,50 @@
 # coding: utf8
 
+
 class BagProblem(object):
     def __init__(self):
         self._cache = {}
 
     def cache(self, f):
-        def _inner(sn, current_weight, chosen):
-            key = (sn, current_weight)
+        def _inner(sequence_number, current_weight, chosen):
+            key = (sequence_number, current_weight)
             if key in self._cache:
+                print("using cached result")
                 return self._cache[key]
-            result = f(sn, current_weight, chosen)
+            result = f(sequence_number, current_weight, chosen)
             self._cache[key] = result
             return result
         return _inner
 
-    def calc(self, goods, totoal_weight):
+    def calc(self, goods, total_weight):
         @self.cache
-        def _calc(sn, current_weight, chosen):
-            if current_weight == totoal_weight or sn == len(goods) - 1:
-                return current_weight, chosen[:]
-            # 装不进去，则不装
-            if current_weight + goods[sn][0] > totoal_weight:
-                return _calc(sn+1, current_weight, chosen[:])
+        def _calc(sequence_number, current_weight, chosen_goods):
+            if current_weight == total_weight or sequence_number == len(goods) - 1:
+                return current_weight, chosen_goods[:]
+
+            # 装不进去
+            if current_weight + goods[sequence_number][0] > total_weight:
+                return _calc(sequence_number + 1, current_weight, chosen_goods[:])
+
             # + 能装进去，分两种情况：
-            # 1，不选择sn
-            totoal_weight1, chosen1 = _calc(sn+1, current_weight, chosen[:])
-            # 2，选择sn
-            totoal_weight2, chosen2 = _calc(sn+1, current_weight+goods[sn][0], chosen+[goods[sn]])
-            if sum([item[1] for item in chosen1]) > sum([item[1] for item in chosen2]):
-                return totoal_weight1, chosen1
-            return totoal_weight2, chosen2
+            # 1，不选择 sequence_number
+            total_weight1, chosen1 = _calc(sequence_number + 1, current_weight, chosen_goods[:])
+            # 2，选择 sequence_number
+            total_weight2, chosen2 = _calc(sequence_number + 1,
+                                           current_weight + goods[sequence_number][0],
+                                           chosen_goods + [goods[sequence_number]])
+            if total_weight1 > total_weight2:
+                return total_weight1, chosen1
+            return total_weight2, chosen2
         return _calc(0, 0, [])
 
-if __name__ == "__main__":
+
+def test():
     bp = BagProblem()
     goods = [(3, 4), (2, 4), (5, 8), (7, 10), (3, 3), (8, 11)]
-    totoal_weight = 15
-    print(bp.calc(goods, totoal_weight))
+    total_weight = 15
+    print(bp.calc(goods, total_weight))
+
+
+if __name__ == "__main__":
+    test()
