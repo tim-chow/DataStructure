50133f1b28c31f1f3f33ee65a3fc7a9f22a3428b
TimChow <jordan23nbastar@vip.qq.com>
2020-07-07 18:35:53 +0800

    Update HeapSort.py

--- 排序算法/堆排序以及topk问题/HeapSort.py
+++ 排序算法/堆排序以及topk问题/HeapSort.py
@@ -1,74 +1,69 @@
-def adjust(array, start, end=None):
-    if end is None:
-        end = len(array)
+# coding: utf8
 
-    while 2*start+1 < end:
-        if 2*start+2 >= end:
-            if array[start] > array[2*start+1]:
-                array[start], array[2*start+1] = array[2*start+1], array[start]
+
+def adjust(array, start, end):
+    if start > end:
+        raise RuntimeError("unreachable")
+
+    while 2 * start + 1 <= end:
+        left = 2 * start + 1
+
+        # 只有左孩子节点
+        if 2 * start + 2 > end:
+            if array[left] < array[start]:
+                array[start], array[left] = \
+                    array[left], array[start]
             break
 
-        element = array[start]
-        left = array[2*start+1]
-        right = array[2*start+2]
-        if element <= left and element <= right:
+        right = 2 * start + 2
+        # 无需调整
+        if array[start] <= array[left] and \
+                array[start] <= array[right]:
             break
 
-        if left < right:
-            array[start], array[2*start+1] = array[2*start+1], array[start]
-            start = 2*start+1
-        else:
-            array[start], array[2*start+2] = array[2*start+2], array[start]
-            start = 2*start+2
+        if array[left] <= array[right]:
+            array[start], array[left] = \
+                array[left], array[start]
+            start = left
+            continue
 
-def heapify(array):
-    mid = len(array) / 2
-    for i in range(mid, -1, -1):
-        adjust(array, i, len(array))
+        array[start], array[right] = \
+            array[right], array[start]
+        start = right
 
-def insert(heap, element):
-    heap.append(element)
-    index = ((len(heap) - 1) - 1) / 2
-    while index >= 0:
-        adjust(heap, index, len(heap))
-        parent = (index - 1) / 2
-        if parent < 0 or heap[parent] <= heap[index]:
-            break
-        index = parent
 
 def heap_sort(array):
-    heapify(array)
+    for ind in range(len(array) / 2, -1, -1):
+        adjust(array, ind, len(array) - 1)
 
-    end = len(array)
-    while end > 1:
-        array[0], array[end-1] = array[end-1], array[0]
-        end = end - 1
-        adjust(array, 0, end)
+    for end in range(len(array) - 1, 0, -1):
+        array[0], array[end] = array[end], array[0]
+        adjust(array, 0, end - 1)
+
 
 def topk(array, k):
-    for ind in range(k/2, -1, -1):
-        adjust(array, ind, k)
+    if k >= len(array):
+        return
+
+    for ind in range(k / 2, -1, -1):
+        adjust(array, ind, k - 1)
 
     for ind in range(k, len(array)):
         if array[ind] <= array[0]:
             continue
-        array[0] = array[ind]
-        adjust(array, 0, k)
-    return array[:k]
 
-def test():
-    import random
-    seq = range(1, 15)
-    random.shuffle(seq)
-    print seq
-    print topk(seq, 7)
+        array[0], array[ind] = array[ind], array[0]
+        adjust(array, 0, k - 1)
 
-    seq = range(1, 15)
-    random.shuffle(seq)
-    print seq
-    heap_sort(seq)
-    print seq
 
 if __name__ == "__main__":
-    test()
-
+    import random
+    elements = list(range(1, 16) * 2)
+    random.shuffle(elements)
+    print("elements are: %s" % elements)
+    heap_sort(elements)
+    print("heap sort: %s" % elements)
+    random.shuffle(elements)
+    print("elements are: %s" % elements)
+    topk(elements, 3)
+    print("top 3: %s" % elements)
