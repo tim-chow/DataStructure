a638ffd29971bb9d6bd58bf8632a0b2e0612574d
TimChow <jordan23nbastar@vip.qq.com>
2020-07-07 23:07:21 +0800

    Update shortest_path.py

--- 分支限界算法/shortest_path.py
+++ 分支限界算法/shortest_path.py
@@ -1,68 +1,66 @@
 # coding: utf8
 
+
 # 最小堆
-class MinHeap:
+class MinHeap(object):
     get_parent = lambda self, ind: (ind - 1) / 2
     get_left_child = lambda self, ind: 2 * ind + 1
     get_right_child = lambda self, ind: 2 * ind + 2
 
     def __init__(self):
-        self._queue = []
+        self._array = []
 
-    def _adjust(self, heap, start=None, end=None):
-        if start == None:
+    def _adjust(self, array, start=None, end=None):
+        if start is None:
             start = 0
-        if end == None:
-            end = len(heap) - 1
+        if end is None:
+            end = len(array) - 1
 
-        index = start
-        while index >= 0:
-            left_child_index = self.get_left_child(index)
-            right_child_index = self.get_right_child(index)
-            if left_child_index > end:
-                break
+        while self.get_left_child(start) <= end:
+            left_child_index = self.get_left_child(start)
+            right_child_index = self.get_right_child(start)
+
             if right_child_index > end:
-                if heap[index] > heap[left_child_index]:
-                    heap[index], heap[left_child_index] = \
-                        heap[left_child_index], heap[index]
+                if array[start] > array[left_child_index]:
+                    array[start], array[left_child_index] = \
+                        array[left_child_index], array[start]
                 break
 
-            if heap[index] <= heap[left_child_index] and \
-                heap[index] <= heap[right_child_index]:
+            if array[start] <= array[left_child_index] and \
+                    array[start] <= array[right_child_index]:
                 break
-            if heap[right_child_index] > heap[left_child_index]:
-                heap[index], heap[left_child_index] = \
-                    heap[left_child_index], heap[index]
-                index = left_child_index
+
+            if array[right_child_index] > array[left_child_index]:
+                array[start], array[left_child_index] = \
+                    array[left_child_index], array[start]
+                start = left_child_index
             else:
-                heap[index], heap[right_child_index] = \
-                    heap[right_child_index], heap[index]
-                index = right_child_index
+                array[start], array[right_child_index] = \
+                    array[right_child_index], array[start]
+                start = right_child_index
 
     def put(self, priority, element):
-        self._queue.append((priority, element))
-        index = len(self._queue) - 1
+        self._array.append((priority, element))
+        index = len(self._array) - 1
         parent = self.get_parent(index)
 
         while parent >= 0:
-            self._adjust(self._queue, parent)
+            self._adjust(self._array, parent)
             parent = self.get_parent(parent)
 
     def get(self):
-        if not self._queue:
+        if not self._array:
             raise RuntimeError("queue is empty")
-        result = self._queue[0]
-        last = self._queue.pop(-1)
-        if self._queue:
-            self._queue[0] = last
-            self._adjust(self._queue, 0)
+        result = self._array[0]
+        last = self._array.pop(-1)
+        if self._array:
+            self._array[0] = last
+            self._adjust(self._array, 0)
         return result
 
-    def size(self):
-        return len(self._queue)
-
     def empty(self):
-        return not len(self._queue)
+        return not len(self._array)
+
 
 # 图的数组/矩阵表示法
 def generate_direct_graph_example():
@@ -83,7 +81,8 @@ def generate_direct_graph_example():
     ]
     return vertexes, arches
 
-def shorted_path(graph, start):
+
+def shortest_path(graph, start):
     queue = MinHeap()
     queue.put(0, start)
     result = {}
@@ -93,19 +92,22 @@ def shorted_path(graph, start):
         for ind, weight in enumerate(graph[1][vertex_index]):
             if not weight:
                 continue
-            weight = current_weight + weight
-            if ind not in result or weight < result[ind]: # 剪枝
-                result[ind] = weight
-                queue.put(weight, ind) # 优先级队列
+            total_weight = current_weight + weight
+            if ind not in result or total_weight < result[ind]:
+                result[ind] = total_weight
+                queue.put(total_weight, ind)
     return result
+
 
 if __name__ == "__main__":
     import pprint
+
     graph = generate_direct_graph_example()
     pprint.pprint(graph)
 
-    result = shorted_path(graph, 0)
-    print "\n".join(["从%s到%s的距离是：%-2d" %
+    result = shortest_path(graph, 0)
+    print(
+        "\n".join(["从%s到%s的距离是：%-3d" %
             (graph[0][0], graph[0][k], v)
-        for k, v in result.items()])
-
+            for k, v in result.items()])
+    )
