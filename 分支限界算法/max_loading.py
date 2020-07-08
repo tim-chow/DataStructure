bddd35c1946e76897537062e15bfba01ba489bac
TimChow <jordan23nbastar@vip.qq.com>
2020-07-08 17:08:45 +0800

    Update max_loading.py

--- 分支限界算法/max_loading.py
+++ 分支限界算法/max_loading.py
@@ -1,5 +1,6 @@
 # coding: utf8
 
+
 class Node(object):
     def __init__(self):
         self.chosen = []
@@ -7,50 +8,58 @@ class Node(object):
 
     def __repr__(self):
         return '%s{chosen=%s, weight=%d}' % (
-            self.__class__.__name__, self.chosen, self.weight)
+            self.__class__.__name__,
+            self.chosen,
+            self.weight)
+
 
 class DummyNode(Node):
     def __init__(self):
         Node.__init__(self)
 
-class MaxLoading:
-    def __init__(self, weight, c):
-        self._w = weight # 集装箱重量数组
-        self._n = len(weight) # 集装箱数量
-        self._c = c # 总载重量
 
-        # 在装了第i个物品之后，剩余物品的总重量
-        self._r = [0] * self._n
-        for ind in range(self._n - 2, -1, -1):
-            self._r[ind] = self._r[ind+1] + self._w[ind+1]
+class MaxLoading(object):
+    def __init__(self, weights, carrying_capacity):
+        self._weights = weights
+        self._number = len(weights)
+        self._carrying_capacity = carrying_capacity
+
+        # 在装了第 i 个物品之后，剩余物品的总重量
+        self._remaining = [0] * self._number
+        for ind in range(self._number - 2, -1, -1):
+            self._remaining[ind] = self._remaining[ind+1] + \
+                self._weights[ind+1]
 
     def max_loading(self):
         queue = [Node(), DummyNode()]
 
-        for i in range(self._n):
+        for ind in range(self._number):
             node = queue.pop(0)
             while not isinstance(node, DummyNode):
-                if node.weight + self._w[i] <= self._c:
+                if node.weight + self._weights[ind] <= self._carrying_capacity:
+                    # 装第 ind 个物品
                     new_node = Node()
-                    new_node.chosen = [item for item in node.chosen] + [i]
-                    new_node.weight = node.weight + self._w[i]
+                    new_node.chosen = [item for item in node.chosen] + [ind]
+                    new_node.weight = node.weight + self._weights[ind]
                     queue.append(new_node)
-                # 剪枝函数
-                # + 如果当前重量 + 当前物品 + 剩余物品的总质量
-                # + + 小于等于 总容量，那么进行剪枝
-                if node.weight + self._w[i] + self._r[i] > self._c:
+
+                if node.weight + self._weights[ind] + \
+                        self._remaining[ind] > self._carrying_capacity:
+                    # 不装第 ind 个物品
                     queue.append(node)
+
                 node = queue.pop(0)
             queue.append(DummyNode())
 
         best = None
         for node in queue[:len(queue)-1]:
-            if not best or node.weight > best.weight:
+            if best is None or node.weight > best.weight:
                 best = node
         return best
 
+
 if __name__ == "__main__":
     import pprint
-    # output: 1 0 1 1
-    pprint.pprint(MaxLoading([20, 10, 26, 15], 70).max_loading())
-
+    pprint.pprint(
+        MaxLoading([20, 23, 26, 24], 70)
+        .max_loading())
