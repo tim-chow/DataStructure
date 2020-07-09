189cc6ea006fb0bef1c99c64f55ce4bb69d0bb4d
TimChow <jordan23nbastar@vip.qq.com>
2020-07-09 16:12:35 +0800

    Update merge_sort_linked_list.py

--- 排序算法/归并排序/merge_sort_linked_list.py
+++ 排序算法/归并排序/merge_sort_linked_list.py
@@ -1,45 +1,69 @@
 # coding: utf8
 
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
     def __str__(self):
-        return "ListNode{val=%s, next=%s}" % (self.val, self.next)
+        return "%s{val=%s, next=%s}" % (
+            self.__class__.__name__,
+            self.val,
+            self.next.val if self.next is not None else self.next
+        )
 
     def __repr__(self):
         return self.__str__() + "@" + hex(id(self))
 
 
 class Solution(object):
-    def sortList(self, head):
-        if head == None or head.next == None:
-            return head
+    def sort(self, h):
+        # 如果 h 中没有元素 或 只有一个元素，则其直接有序，立刻返回
+        if h is None or h.next is None:
+            return h
 
-        mid = head
-        tail = head
-        while tail.next != None:
+        # 通过快慢指针，找到链表的中间节点
+        tail = h
+        mid = h
+        while tail.next is not None:
+            # tail 先前进一步
             tail = tail.next
-            if tail.next != None:
+
+            # 如果未到尾部，tail 再前进一步，mid 前进一步
+            if tail.next is not None:
                 tail = tail.next
                 mid = mid.next
 
+            # 每次循环 tail 前进两步，mid 前进一步，所以当 tail 到达尾部时，
+            # mid 到达链表的中间位置
+
+        # 将列表从中间分成两个：head、right
         right = mid.next
         mid.next = None
 
-        head1 = self.sortList(head)
-        head2 = self.sortList(right)
-        return self._merge(head1, head2).next
+        # 使用归并排序分别对着两个列表进行排序
+        l1 = self.sort(h)
+        l2 = self.sort(right)
 
-    def _merge(self, l1, l2):
-        head = ListNode(None)
-        tail = head
+        # 归并两个有序链表
+        return self.merge(l1, l2)
+
+    @staticmethod
+    def merge(l1, l2):
+        """将两个有序链表归并成一个有序列表"""
+        if l1 is l2:
+            return l1
+
+        # h 是头节点
+        h = ListNode(None)
+
+        tail = h
         x = l1
         y = l2
 
-        while x != None and y != None:
-            if x.val < y.val:
+        while x is not None and y is not None:
+            if x.val <= y.val:
                 tail.next = x
                 tail = x
                 x = x.next
@@ -48,28 +72,35 @@ class Solution(object):
                 tail = y
                 y = y.next
 
-        if x != None:
+        if x is not None:
             tail.next = x
-        if y != None:
+        elif y is not None:
             tail.next = y
 
-        return head
+        return h.next
 
 
 if __name__ == "__main__":
-    tmp = None
-    head = None
-    for element in [4, 2, 1, 3]:
-        node = ListNode(element)
-        if head == None:
-            head = node
-            tmp = head
-        else:
-            tmp.next = node
-            tmp = node
+    import random
 
-    head = Solution().sortList(head)
-    tmp = head
-    while tmp != None:
-        print(tmp)
-        tmp = tmp.next
+    elements = list(range(-10, 11))
+    random.shuffle(elements)
+    print(elements)
+
+    # 生成一个无序链表
+    temp1 = None
+    head = None
+    for element in elements:
+        node = ListNode(element)
+        if head is None:
+            head = node
+            temp1 = node
+        else:
+            temp1.next = node
+            temp1 = node
+
+    new_head = Solution().sort(head)
+    temp2 = new_head
+    while temp2 is not None:
+        print(temp2.val)
+        temp2 = temp2.next
