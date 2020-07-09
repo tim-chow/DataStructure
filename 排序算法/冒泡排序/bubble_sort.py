e33b9ad03e0f0bb26d6ecc589781ca370b043eb4
TimChow <jordan23nbastar@vip.qq.com>
2020-07-09 20:12:41 +0800

    Update bubble_sort.py

--- 排序算法/冒泡排序/bubble_sort.py
+++ 排序算法/冒泡排序/bubble_sort.py
@@ -1,20 +1,24 @@
-class BubbleSort:
-    @staticmethod
-    def bubble_sort(array):
-        size = len(array)
-        for i in range(size-1, 0, -1):
-            swapped = False
-            for j in range(i):
-                if array[j] > array[j+1]:
-                    swapped = True
-                    array[j], array[j+1] = array[j+1], array[j]
-            if not swapped:
-                break
+# coding: utf8
+
+
+def bubble_sort(array):
+    swapped = True
+    for end in range(len(array) - 1, 0, -1):
+        if not swapped:
+            return
+        for ind in range(0, end):
+            if array[ind] > array[ind + 1]:
+                swapped = True
+                array[ind], array[ind + 1] = \
+                    array[ind + 1], array[ind]
+
 
 if __name__ == "__main__":
     import random
-    lst = range(20)
-    random.shuffle(lst)
-    print lst
-    BubbleSort.bubble_sort(lst)
-    print lst
+
+    elements = list(range(20)) * 2
+    random.shuffle(elements)
+    print(elements)
+
+    bubble_sort(elements)
+    print(elements)
