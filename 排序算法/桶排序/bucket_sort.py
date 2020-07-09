8652356e0a2da818a1c65cdb22b9b6947867abd8
TimChow <jordan23nbastar@vip.qq.com>
2020-07-10 01:12:50 +0800

    Update bucket_sort.py

--- 排序算法/桶排序/bucket_sort.py
+++ 排序算法/桶排序/bucket_sort.py
@@ -1,41 +1,67 @@
-class BucketSort:
-    @classmethod
-    def bucket_sort(cls, array, m, devide, sort):
-        n = len(array)
-        buckets = [[] for _ in range(m)]
+# coding: utf8
 
-        for element in array:
-            buckets[devide(element)].append(element)
 
-        for bucket in buckets:
-            sort(bucket)
+def bucket_sort(array, m, divide_func, sort_func):
+    n = len(array)
 
-        cursor = 0
-        for i in range(m):
-            for element in buckets[i]:
-                array[cursor] = element
-                cursor = cursor + 1
+    # 创建桶
+    buckets = [([None] * n) for _ in range(m)]
+
+    bucket_index_to_cursor = {}
+    # 将元素放到相应的桶中
+    for element in array:
+        bucket_index = divide_func(element)
+        if bucket_index not in bucket_index_to_cursor:
+            bucket_index_to_cursor[bucket_index] = 0
+        else:
+            bucket_index_to_cursor[bucket_index] = \
+                bucket_index_to_cursor[bucket_index] + 1
+        buckets[bucket_index][bucket_index_to_cursor[bucket_index]] = element
+
+    # 对每个桶内的数据元素进行排序
+    for bucket_index, last_index in bucket_index_to_cursor.items():
+        sort_func(buckets[bucket_index], 0, last_index)
+
+    # 将桶中的元素复制回序列
+    cursor = 0
+    for bucket_index, last_index in sorted(bucket_index_to_cursor.items(), key=lambda k: k):
+        bucket = buckets[bucket_index]
+        for index in range(last_index + 1):
+            array[cursor] = bucket[index]
+            cursor = cursor + 1
+
 
 if __name__ == "__main__":
-    def bubble_sort(array):
-        for i in range(len(array)-1):
-            for j in range(i+1, len(array)):
-                if array[j] < array[i]:
-                    array[i], array[j] = array[j], array[i]
+    import random
 
-    def devide(element):
-        if element < 10:
+
+    def divide(element):
+        if element < 5:
             return 0
-        if element < 15:
+        if element < 10:
             return 1
-        if element < 20:
+        if element < 15:
             return 2
         return 3
 
-    import random
-    lst = range(25)
-    random.shuffle(lst)
-    print lst
-    BucketSort.bucket_sort(lst, 4, devide, bubble_sort)
-    print lst
+    # 直接插入排序
+    def sort(array, start, end):
+        if start >= end:
+            return
+        for i in range(start + 1, end + 1):
+            for j in range(0, i):
+                if array[j] <= array[i]:
+                    continue
+                temp = array[i]
+                # 把 array[j...i-1] 移动到 array[j+1...i]
+                for ind in range(i, j, -1):
+                    array[ind] = array[ind - 1]
+                array[j] = temp
+                break
 
+    elements = list(range(20)) * 2
+    random.shuffle(elements)
+    print(elements)
+
+    bucket_sort(elements, 4, divide, sort)
+    print(elements)
