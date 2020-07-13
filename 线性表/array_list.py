0e9a9b9ede52916310cacf7c162ca0c2ecb312fd
TimChow <jordan23nbastar@vip.qq.com>
2020-07-13 17:13:23 +0800

    Update array_list.py

--- 线性表/array_list.py
+++ 线性表/array_list.py
@@ -1,10 +1,17 @@
-#coding: utf8
+# coding: utf8
+
 
 class ArrayList(object):
+    """
+    顺序表实现
+    """
     def __init__(self, initialize_size=16, capacity_factor=2):
+        """
+        :param initialize_size: 初始容量
+        :param capacity_factor: 扩容因子
+        """
         self._initialize_size = initialize_size
         self._capacity_factor = capacity_factor
-
         self._array = [None] * initialize_size
         self._total_size = initialize_size
         self._used_size = 0
@@ -12,8 +19,11 @@ class ArrayList(object):
     def _ensure_capacity(self, increased_size):
         self._total_size = self._total_size + increased_size
         new_array = [None] * self._total_size
+
+        # 将旧数组中的元素拷贝到新数组
         for ind in range(self._used_size):
             new_array[ind] = self._array[ind]
+
         self._array = new_array
 
     def _will_ensure_capacity(self):
@@ -24,13 +34,16 @@ class ArrayList(object):
 
     def insert(self, index, element):
         self._will_ensure_capacity()
+
         if index < 0:
             index = 0
-        if index >= self._used_size:
+        if index > self._used_size:
             index = self._used_size
 
+        # 将 array[index...used_size-1] 移动到 array[index+1...used_size]
         for ind in range(self._used_size, index, -1):
-            self._array[ind] = self._array[ind-1]
+            self._array[ind] = self._array[ind - 1]
+
         self._array[index] = element
         self._used_size = self._used_size + 1
 
@@ -39,27 +52,18 @@ class ArrayList(object):
             raise IndexError("invalid index")
 
         removed_element = self._array[index]
-        # move [index+1 ... used_size-1] ---> [index, used_size-2]
-        for i in range(index, self._used_size - 2 + 1):
-            self._array[i] = self._array[i+1]
+        # 将 array[index+1...used_size-1] 移动到 array[index, used_size-2]
+        for ind in range(index, self._used_size - 2 + 1):
+            self._array[ind] = self._array[ind + 1]
 
         self._used_size = self._used_size - 1
         return removed_element
-
-    def add(self, element):
-        self._will_ensure_capacity()
-        self._array[self._used_size] = element
-        self._used_size = self._used_size + 1
-        return self
 
     def set(self, index, element):
         self._array[index] = element
 
     def get(self, index):
         return self._array[index]
-
-    def __str__(self):
-        return ", ".join(map(str, self._array[: self._used_size]))
 
     def size(self):
         return self._used_size
@@ -70,7 +74,8 @@ class ArrayList(object):
                 return index
         return -1
 
-class Iterator:
+
+class Iterator(object):
     def __init__(self, collection):
         self._collection = collection
         self._cursor = 0
@@ -82,8 +87,7 @@ class Iterator:
         self._cursor = 0
 
     def next(self):
-        size = self._collection.size()
-        if self._cursor >= size:
+        if self._cursor >= self._collection.size():
             raise StopIteration("over")
 
         try:
@@ -91,11 +95,11 @@ class Iterator:
         finally:
             self._cursor = self._cursor + 1
 
-if __name__ == "__main__":
-    list = ArrayList(4, 2)
-    list.insert(0, 100)
-    list.insert(0, 50)
-    list.insert(-1, 0)
-    list.insert(100, 150)
-    print list.find(150)
 
+if __name__ == "__main__":
+    array_list = ArrayList(4, 2)
+    array_list.insert(0, 100)
+    array_list.insert(0, 50)
+    array_list.insert(-1, 0)
+    array_list.insert(100, 150)
+    print(array_list.find(150))
