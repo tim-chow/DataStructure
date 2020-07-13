9a2675c031476e213bad3f316eb4b8235ca4ca3e
TimChow <jordan23nbastar@vip.qq.com>
2020-07-13 13:49:42 +0800

    Update MFSet.py

--- 树/树与等价关系/MFSet.py
+++ 树/树与等价关系/MFSet.py
@@ -1,55 +1,59 @@
-class Node:
-    def __init__(self, element, parent=-1):
-        self._element = element
-        self._parent = parent
+# coding: utf8
 
-    def set_parent(self, parent):
-        self._parent = parent
 
-    def get_parent(self):
-        return self._parent
-
-    def get_element(self):
-        return self._element
-
-    def find_root(self, nodes):
-        index = nodes.index(self)
-        node = self
-        while node.get_parent() >= 0:
-            index = node.get_parent()
-            node = nodes[index]
-        return node, index
-
-    def merge(self, another, nodes):
-        another_root, another_index = another.find_root(nodes)
-        root, index = self.find_root(nodes)
-        if another_root == root:
-            return
-
-        another_nodes_count = -1 * another_root.get_parent()
-        nodes_count = -1 * root.get_parent()
-
-        if another_nodes_count > nodes_count:
-            root.set_parent(another_index)
-            another_root.set_parent(-1*(another_nodes_count + nodes_count))
-        else:
-            another_root.set_parent(index)
-            root.set_parent(-1*(another_nodes_count + nodes_count))
+class Node(object):
+    def __init__(self, element):
+        self.element = element
+        self.parent = -1
 
     def __str__(self):
-        return "Node[element={0._element}, parent={0._parent}]".format(self)
+        return "%s{element=%s, parent=%d}" % (
+            self.__class__.__name__,
+            self.element,
+            self.parent)
+
     __repr__ = __str__
 
-def main():
-    R = [(0, 2), (2, 4), (4, 6), (6, 8)]
-    S = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    nodes = [Node(element, -1) for element in S]
 
-    for x, y in R:
-        nodes[x].merge(nodes[y], nodes)
+class MFSet(object):
+    def __init__(self, s, r):
+        self._nodes = [Node(e) for e in s]
+        self._r = r
+        self.divide()
 
-    print nodes
+    def find_root(self, x):
+        temp = x
+        while self._nodes[temp].parent >= 0:
+            temp = self._nodes[temp].parent
+        return temp
+
+    def merge(self, x, y):
+        si = self.find_root(x)
+        sj = self.find_root(y)
+        if si == sj:
+            return
+
+        node_si = self._nodes[si]
+        node_sj = self._nodes[sj]
+
+        if -1 * node_si.parent >= -1 * node_sj.parent:
+            node_si.parent = node_si.parent + node_sj.parent
+            node_sj.parent = si
+        else:
+            node_sj.parent = node_si.parent + node_sj.parent
+            node_si.parent = sj
+
+    def divide(self):
+        for x, y in self._r:
+            self.merge(x, y)
+
+    @property
+    def nodes(self):
+        return self._nodes
+
 
 if __name__ == "__main__":
-    main()
-
+    s = [0, 1, 2, 3, 4, 5]
+    r = [(0, 2), (2, 4), (1, 3), (3, 5)]
+    mfset = MFSet(s, r)
+    print(mfset.nodes)
