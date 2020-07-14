c431db1a670f8c3e96edc28e667d8e65f4caf390
TimChow <744475502@qq.com>
2020-07-14 13:35:06 +0800

    Update reverse_list.py

--- 线性表/逆转单链表/reverse_list.py
+++ 线性表/逆转单链表/reverse_list.py
@@ -1,28 +1,51 @@
+# coding: utf8
+
+
 class Node(object):
-    def __init__(self, element=None, next=None):
+    def __init__(self, element=None, next_node=None):
         self.element = element
-        self.next = next
-
-    def __str__(self):
-        return "Node{element=%s, next=%s}" % (
-            self.element, self.next)
+        self.next_node = next_node
 
 
-def reverse_list(first_node):
-    prev_node = None
-    tmp_node = first_node
-    while tmp_node != None:
-        p = tmp_node.next
-        tmp_node.next = prev_node
-        prev_node = tmp_node
-        tmp_node = p
-    return prev_node
+def reverse_list(node):
+    if node is None:
+        return node
+
+    p1 = node
+    p2 = p1.next_node
+    p1.next_node = None
+
+    while p2 is not None:
+        p3 = p2.next_node
+        p2.next_node = p1
+        p1 = p2
+        p2 = p3
+
+    return p1
 
 
 if __name__ == "__main__":
-    node1 = Node(1)
-    node1.next = node2 = Node(2)
-    node2.next = node3 = Node(3)
-    print(node1)
-    node = reverse_list(node1)
-    print(node)
+    import unittest
+
+    class TestReverseList(unittest.TestCase):
+        def testReverseList(self):
+            nodes = [Node(ind, None) for ind in range(10)]
+            for ind in range(9):
+                nodes[ind].next_node = nodes[ind + 1]
+            node = nodes[0]
+            del nodes
+
+            temp = node
+            while temp is not None:
+                print(temp.element)
+                temp = temp.next_node
+
+            print("=" * 10)
+
+            head = reverse_list(node)
+            temp = head
+            while temp is not None:
+                print(temp.element)
+                temp = temp.next_node
+
+    unittest.main()
