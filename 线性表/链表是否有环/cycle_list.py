f036f6f0fb1d430ea71365631ac0acb149a95cfa
TimChow <744475502@qq.com>
2020-07-14 13:16:58 +0800

    Update cycle_list.py

--- 线性表/链表是否有环/cycle_list.py
+++ 线性表/链表是否有环/cycle_list.py
@@ -1,66 +1,96 @@
+# coding: utf8
+
+
 class Node(object):
-    def __init__(self, element=None, next=None):
+    def __init__(self, element=None, next_node=None):
         self.element = element
-        self.next = next
+        self.next_node = next_node
 
-    def __str__(self):
-        return "Node[element={0.element}]".format(self)
 
-def has_cycle(linked_list):
-    fast = slow = linked_list
-    while fast != None:
-        fast = fast.next
-        if fast != None:
-            fast = fast.next
-            slow = slow.next
-            if fast == slow:
+def has_cycle(node):
+    """判断单链表是否有环"""
+    if node is None:
+        return False
+
+    slow = fast = node
+    while fast is not None:
+        # 快指针先前进一步
+        fast = fast.next_node
+        if fast is not None:
+            # 快指针再前进一步
+            fast = fast.next_node
+            # 慢指针前进一步
+            slow = slow.next_node
+
+            # 快慢指针相遇表示有环
+            if slow is fast:
                 return True
     return False
 
-def cycle_length(linked_list):
-    fast = slow = linked_list
+
+def get_cycle_length(node):
+    """获取环的长度"""
+    if node is None:
+        return 0
+
+    slow = fast = node
     meet_count = 0
     loop_count = 0
-    while fast != None:
-        fast = fast.next
-        if fast != None:
+    while fast is not None:
+        fast = fast.next_node
+        if fast is not None:
+            fast = fast.next_node
+            slow = slow.next_node
             loop_count = loop_count + 1
-            fast = fast.next
-            slow = slow.next
-            if fast == slow:
+            if slow is fast:
                 meet_count = meet_count + 1
                 if meet_count == 1:
+                    # 快慢指针第一次相遇
                     loop_count = 0
-                else:
+                elif meet_count == 2:
+                    # 快慢指针再次相遇时，慢指针走的步数就是环长
                     return loop_count
-    return -1
+    return 0
 
-def joint_node(linked_list):
-    fast = slow = linked_list
-    meet_node = None
-    while fast != None:
-        fast = fast.next
-        if fast != None:
-            fast = fast.next
-            slow = slow.next
-            if fast == slow:
-                meet_node = fast
-                break
-    if meet_node is None:
-        raise RuntimeError("no cycle in linked list")
 
-    p = linked_list
-    while p != meet_node:
-        p = p.next
-        meet_node = meet_node.next
-    return p
+def get_joint_node(node):
+    if node is None:
+        return None
+
+    slow = fast = node
+    while fast is not None:
+        fast = fast.next_node
+        if fast is not None:
+            fast = fast.next_node
+            slow = slow.next_node
+            if slow is fast:
+                p1 = slow
+                p2 = node
+                while p1 is not p2:
+                    p1 = p1.next_node
+                    p2 = p2.next_node
+                return p1
+
+    return None
+
 
 if __name__ == "__main__":
-    node1 = Node(1)
-    node1.next = node2 = Node(2)
-    node2.next = node3 = Node(3)
-    node3.next = node4 = Node(4)
-    node4.next = node3
-    print(has_cycle(node1))
-    print(cycle_length(node1))
-    print(joint_node(node1))
+    import unittest
+
+    class CycleListTest(unittest.TestCase):
+        def setUp(self):
+            # 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 2
+            nodes = [Node(ind) for ind in range(6)]
+            for ind in range(5):
+                nodes[ind].next_node = nodes[ind + 1]
+            nodes[5].next_node = nodes[2]
+            self.node = nodes[0]
+            # 连接点
+            self.joint_node = nodes[2]
+
+        def testCycleList(self):
+            self.assertTrue(has_cycle(self.node))
+            self.assertEqual(get_cycle_length(self.node), 4)
+            self.assertIs(get_joint_node(self.node), self.joint_node)
+
+    unittest.main()
