1e6842f862b4046b88f16d5ba60129447ac6fe0c
TimChow <744475502@qq.com>
2020-07-16 15:28:25 +0800

    Update avl_tree.py

--- 树/二叉树/平衡二叉树/avl_tree.py
+++ 树/二叉树/平衡二叉树/avl_tree.py
@@ -1,6 +1,10 @@
 # coding: utf8
 
+
 class Node(object):
+    """
+    AVL 树的节点，使用三叉链表表示法
+    """
     def __init__(self, keyword, parent=None, balance_factor=0):
         self.keyword = keyword
         self.parent = parent
@@ -8,50 +12,48 @@ class Node(object):
         self.right = None
         self.balance_factor = balance_factor
 
-    def __str__(self):
-        return "Node{keyword=%s, parent=%s, left=%s, right=%s, bf=%d}" % (
-            self.keyword, self.parent and self.parent.keyword,
-                self.left, self.right, self.balance_factor)
-
-    __repr__ = __str__
-
 
 class AVLTree(object):
+    """
+    AVL 树实现
+    """
     def __init__(self):
-        self._root_node = None
+        self._root = None
 
     @property
-    def root_node(self):
-        return self._root_node
+    def root(self):
+        return self._root
 
     def insert_keyword(self, keyword):
-        if self._root_node == None:
-            self._root_node = Node(keyword)
+        """
+        向 AVL 树中插入关键字
+        """
+        if self._root is None:
+            self._root = Node(keyword)
             return
 
-        node = self._root_node
-        while node != None:
+        node = self._root
+        while True:
             if keyword < node.keyword:
-                if node.left == None:
+                if node.left is None:
                     node.left = Node(keyword, node)
                     self.rotate(node.left)
                     break
                 node = node.left
             else:
-                if node.right == None:
+                if node.right is None:
                     node.right = Node(keyword, node)
                     self.rotate(node.right)
                     break
                 node = node.right
 
-    def find_unbalance_node(self, node):
-        if node.parent:
-            # 用于标记插入发生在左子树还是右子树
-            is_left = node is node.parent.left
+    @classmethod
+    def find_unbalance_node(cls, node):
+        """"寻找第一个失衡点"""
+        left = node is node.parent.left
         node = node.parent
-
-        while node != None:
-            if is_left:
+        while node is not None:
+            if left:
                 if node.balance_factor == -1:
                     node.balance_factor = 0
                     return None
@@ -67,8 +69,8 @@ class AVLTree(object):
                     node.balance_factor = -2
                     return node
                 node.balance_factor = -1
-            if node.parent:
-                is_left = node is node.parent.left
+            if node.parent is not None:
+                left = node is node.parent.left
             node = node.parent
 
     def rotate_ll(self, node):
@@ -78,8 +80,8 @@ class AVLTree(object):
         BR = B.right
 
         B.parent = parent
-        if parent == None:
-            self._root_node = B
+        if parent is None:
+            self._root = B
         else:
             if node is parent.left:
                 parent.left = B
@@ -104,8 +106,8 @@ class AVLTree(object):
         CR = C.right
 
         C.parent = parent
-        if parent == None:
-            self._root_node = C
+        if parent is None:
+            self._root = C
         else:
             if node is parent.left:
                 parent.left = C
@@ -122,7 +124,7 @@ class AVLTree(object):
         A.parent = C
         A.left = CR
         if CR:
-            CR.parent = A 
+            CR.parent = A
 
         if C.balance_factor == -1:
             A.balance_factor = 0
@@ -144,8 +146,8 @@ class AVLTree(object):
         BL = B.left
 
         B.parent = parent
-        if parent == None:
-            self._root_node = B
+        if parent is None:
+            self._root = B
         else:
             if node is parent.left:
                 parent.left = B
@@ -170,8 +172,8 @@ class AVLTree(object):
         CR = C.right
 
         C.parent = parent
-        if parent == None:
-            self._root_node = C
+        if parent is None:
+            self._root = C
         else:
             if node is parent.left:
                 parent.left = C
@@ -207,48 +209,34 @@ class AVLTree(object):
         unbalance_node = self.find_unbalance_node(node)
         if unbalance_node is None:
             return
-        left_child = unbalance_node.left
-        right_child = unbalance_node.right
+        left = unbalance_node.left
+        right = unbalance_node.right
         if unbalance_node.balance_factor > 0:
-            if left_child.balance_factor > 0:
+            if left.balance_factor > 0:
                 self.rotate_ll(unbalance_node)
             else:
                 self.rotate_lr(unbalance_node)
         else:
-            if right_child.balance_factor < 0:
+            if right.balance_factor < 0:
                 self.rotate_rr(unbalance_node)
             else:
                 self.rotate_rl(unbalance_node)
 
-    def bfs(self):
-        if not self._root_node:
-            return []
-
-        queue = [self._root_node]
-        cursor = 0
-        while cursor < len(queue):
-            node = queue[cursor]
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            cursor = cursor + 1
-        return queue
-
     @classmethod
     def get_height(cls, node):
-        if node == None:
+        if node is None:
             return 0
-        if node.left == None and node.right == None:
+        if node.left is None and node.right is None:
             return 1
         return max(cls.get_height(node.left), cls.get_height(node.right)) + 1
 
     def delete_keyword(self, keyword):
-        if not self._root_node:
-            return
-
-        node = self._root_node
-        while node != None:
+        """
+        从 AVL 树中删除节点
+        """
+        # 找到待删除节点
+        node = self._root
+        while node is not None:
             if node.keyword == keyword:
                 break
             if node.keyword > keyword:
@@ -258,36 +246,43 @@ class AVLTree(object):
         else:
             return
 
-        if node is self._root_node and (node.left == None or node.right == None):
-            self._root_node = node.left or node.right
+        # 如果待删除节点是根节点，并且其左子树或右子树为空，则将其非空子树接到根节点即可
+        if node is self._root and (node.left is None or node.right is None):
+            self._root = node.left or node.right
             return
 
+        # 否则，找到左子树的最右节点，并使用其关键字替代待删除节点的关键字，然后将该节点删掉
         real_deleted_node = node
-        if node.left != None:
+        if node.left is not None:
             real_deleted_node = node.left
-            while real_deleted_node.right != None:
+            while real_deleted_node.right is not None:
                 real_deleted_node = real_deleted_node.right
         node.keyword = real_deleted_node.keyword
-        self.rotate2(real_deleted_node)
+        self.delete_and_rotate(real_deleted_node)
 
-    def rotate2(self, real_deleted_node):
+    def delete_and_rotate(self, real_deleted_node):
+        # 将真正删除的节点的非空子树接到其父节点
         parent = real_deleted_node.parent
         subtree = real_deleted_node.left or real_deleted_node.right
         if real_deleted_node is parent.left:
             parent.left = subtree
-            is_left = True
+            left = True
         else:
             parent.right = subtree
-            is_left = False
-        if subtree != None:
+            left = False
+        if subtree is not None:
             subtree.parent = parent
+
+        # 从真正删除的节点的父节点开始，向根节点方向前进，调整所经过的节点的平衡因子
         node = parent
-        while node != None:
-            tmp = node.parent
-            tmp_is_left = False
-            if tmp:
-                tmp_is_left = node is tmp.left
-            if is_left:
+        while node is not None:
+            # 旋转之后 node 的父节点会发生改变，所以提前保存其父节点
+            next_node = node.parent
+            next_left = False
+            if next_node is not None:
+                next_left = node is next_node.left
+
+            if left:
                 if node.balance_factor == 1:
                     node.balance_factor = 0
                 elif node.balance_factor == 0:
@@ -296,39 +291,41 @@ class AVLTree(object):
                 else:
                     node.balance_factor = -2
                     if node.right.balance_factor > 0:
-                        self.rotate_rl2(node)
+                        self.rotate_rl_when_delete(node)
                     elif node.right.balance_factor == 0:
-                        self.rotate_rr2(node)
+                        # 旋转之后子树高度不变，无需继续向上调整
+                        self.rotate_rr_when_delete(node)
                         break
                     else:
-                        self.rotate_rr2(node)
+                        self.rotate_rr_when_delete(node)
             else:
                 if node.balance_factor == 1:
                     node.balance_factor = 2
                     if node.left.balance_factor < 0:
-                        self.rotate_lr2(node)
+                        self.rotate_lr_when_delete(node)
                     elif node.left.balance_factor == 0:
-                        self.rotate_ll2(node)
+                        self.rotate_ll_when_delete(node)
                         break
                     else:
-                        self.rotate_ll2(node)
+                        self.rotate_ll_when_delete(node)
                 elif node.balance_factor == 0:
                     node.balance_factor = 1
                     break
                 else:
                     node.balance_factor = 0
-            node = tmp
-            is_left = tmp_is_left
 
-    def rotate_ll2(self, node):
+            node = next_node
+            left = next_left
+
+    def rotate_ll_when_delete(self, node):
         parent = node.parent
         A = node
         B = A.left
         BR = B.right
 
         B.parent = parent
-        if parent == None:
-            self._root_node = B
+        if parent is None:
+            self._root = B
         else:
             if node is parent.left:
                 parent.left = B
@@ -348,7 +345,7 @@ class AVLTree(object):
             A.balance_factor = 0
             B.balance_factor = 0
 
-    def rotate_lr2(self, node):
+    def rotate_lr_when_delete(self, node):
         parent = node.parent
         A = node
         B = A.left
@@ -357,8 +354,8 @@ class AVLTree(object):
         CR = C.right
 
         C.parent = parent
-        if parent == None:
-            self._root_node = C
+        if parent is None:
+            self._root = C
         else:
             if node is parent.left:
                 parent.left = C
@@ -390,7 +387,7 @@ class AVLTree(object):
             B.balance_factor = 0
             C.balance_factor = 0
 
-    def rotate_rl2(self, node):
+    def rotate_rl_when_delete(self, node):
         parent = node.parent
         A = node
         B = A.right
@@ -399,8 +396,8 @@ class AVLTree(object):
         CR = C.right
 
         C.parent = parent
-        if parent == None:
-            self._root_node = C
+        if parent is None:
+            self._root = C
         else:
             if node is parent.left:
                 parent.left = C
@@ -432,15 +429,15 @@ class AVLTree(object):
             B.balance_factor = 0
             C.balance_factor = 0
 
-    def rotate_rr2(self, node):
+    def rotate_rr_when_delete(self, node):
         parent = node.parent
         A = node
         B = node.right
         BL = B.left
 
         B.parent = parent
-        if parent == None:
-            self._root_node = B
+        if parent is None:
+            self._root = B
         else:
             if node is parent.left:
                 parent.left = B
@@ -461,50 +458,44 @@ class AVLTree(object):
             B.balance_factor = 0
 
     @classmethod
-    def is_valid_avl_tree(cls, root_node):
-        if not root_node:
+    def is_valid_avl_tree(cls, root):
+        if root is None:
             return 0
-        if root_node.balance_factor not in [-1, 0, 1]:
+        if root.balance_factor not in [-1, 0, 1]:
             return 1
-        if root_node.balance_factor != (
-            cls.get_height(root_node.left) -
-                cls.get_height(root_node.right)):
+        if root.balance_factor != (
+                cls.get_height(root.left) -
+                cls.get_height(root.right)):
             return 2
-        if root_node.left and root_node.left.keyword >= root_node.keyword:
+
+        if root.left is not None and root.left.keyword > root.keyword:
             return 3
-        if root_node.right and root_node.right.keyword < root_node.keyword:
+        if root.right is not None and root.right.keyword < root.keyword:
             return 4
 
-        left_status = cls.is_valid_avl_tree(root_node.left)
+        left_status = cls.is_valid_avl_tree(root.left)
         if left_status != 0:
             return left_status
-        right_status = cls.is_valid_avl_tree(root_node.right)
+        right_status = cls.is_valid_avl_tree(root.right)
         if right_status != 0:
             return right_status
         return 0
 
+
 if __name__ == "__main__":
     import random
-    min_keyword = 1
-    max_keyword = random.choice(range(50, 100))
-    array = range(min_keyword, max_keyword)
+
+    array = list(range(100))
     random.shuffle(array)
     print(array)
     tree = AVLTree()
     for keyword in array:
         tree.insert_keyword(keyword)
-
-    print(tree.root_node)
-    status = AVLTree.is_valid_avl_tree(tree.root_node)
-    print(status == 0)
+        status = AVLTree.is_valid_avl_tree(tree.root)
+        assert status == 0, "tree is not AVL"
 
     random.shuffle(array)
     for keyword in array:
-        print("delete keyword: %s" % keyword)
         tree.delete_keyword(keyword)
-        status = AVLTree.is_valid_avl_tree(tree.root_node)
-        print(status == 0)
-        if status != 0:
-            print(tree.root_node)
-    print(tree.root_node)
-
+        status = AVLTree.is_valid_avl_tree(tree.root)
+        assert status == 0, "tree is not AVL"
