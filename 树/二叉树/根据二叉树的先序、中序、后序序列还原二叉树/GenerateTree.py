86360a37ab7e62e52dbf296d56860e8f13338e23
TimChow <744475502@qq.com>
2020-07-20 02:30:33 +0800

    Update GenerateTree.py

--- 树/二叉树/根据二叉树的先序、中序、后序序列还原二叉树/GenerateTree.py
+++ 树/二叉树/根据二叉树的先序、中序、后序序列还原二叉树/GenerateTree.py
@@ -1,143 +1,146 @@
-import types
-import threading
+# coding: utf8
 
-class NodeFactory:
-    map = {}
-    lock = threading.Lock()
-
-    @classmethod
-    def factory(cls, element):
-        with cls.lock:
-            if not element in cls.map:
-                cls.map[element] = Node(element)
-            return cls.map[element]
 
 class Node(object):
-    def __init__(self, element, left=None, right=None):
-        self._element = element
-        self._left = left
-        self._right = right
+    def __init__(self, keyword, left=None, right=None):
+        self.keyword = keyword
+        self.left = left
+        self.right = right
 
-    @property
-    def element(self):
-        return self._element
 
-    @property
-    def left(self):
-        return self._left
-
-    @property
-    def right(self):
-        return self._right
-
-    def set_left(self, left):
-        if isinstance(left, (types.NoneType, Node)):
-            self._left = left
-        return self
-
-    def set_right(self, right):
-        if isinstance(right, (types.NoneType, Node)):
-            self._right = right
-        return self
-
-    def postorder_traverse(self):
-        nodes = []
-        if self.left:
-            nodes.extend(self.left.postorder_traverse())
-        if self.right:
-            nodes.extend(self.right.postorder_traverse())
-        nodes.append(self.element)
-        return nodes
-
-    def inorder_traverse(self):
-        nodes = []
-        if self.left:
-            nodes.extend(self.left.inorder_traverse())
-        nodes.append(self.element)
-        if self.right:
-            nodes.extend(self.right.inorder_traverse())
-        return nodes
-
-    def preorder_traverse(self):
-        nodes = [self.element]
-        if self.left:
-            nodes.extend(self.left.preorder_traverse())
-        if self.right:
-            nodes.extend(self.right.preorder_traverse())
-        return nodes
-    
-    @staticmethod
-    def find_pos(sequence, element):
-        for ind, one_element in enumerate(sequence):
-            if one_element == element:
-                return ind
-        return -1
+class NodeFactory(object):
+    nodes = {}
 
     @classmethod
-    def generate_tree(cls, preorder, inorder, postorder):
-        if len(preorder) == 0 or \
-            len(preorder) != len(inorder) or \
-            len(inorder) != len(postorder) or \
-            len(preorder) != len(postorder):
-            raise RuntimeError("invalid input")
-        if len(preorder) == 1:
-            return NodeFactory.factory(preorder[0])
+    def get_node(cls, keyword):
+        if keyword not in cls.nodes:
+            cls.nodes[keyword] = Node(keyword)
+        return cls.nodes[keyword]
 
-        left_preorder, left_inorder, left_postorder = \
-            [], [], []
-        right_preorder, right_inorder, right_postorder = \
-            [], [], []
-        root = NodeFactory.factory(preorder[0])
-        if preorder[0] == inorder[0]:
-            root.set_left(None)
-            right_preorder = preorder[1:]
-            right_inorder = inorder[1:]
-            right_postorder = postorder[:len(postorder)-1]
-        else:
-            root.set_left(NodeFactory.factory(
-                preorder[1]))
 
-        if preorder[0] == inorder[-1]:
-            root.set_right(None)
-            left_preorder = preorder[1:]
-            left_inorder = inorder[:len(inorder)-1]
-            left_postorder = postorder[:len(postorder)-1]
-        else:
-            root.set_right(NodeFactory.factory(
-                postorder[-2]))
+def find_pos(sequence, target):
+    for pos, element in enumerate(sequence):
+        if target == element:
+            return pos
+    return -1
 
-        if root.left and root.right:
-            pos = cls.find_pos(preorder, postorder[-2]) 
-            left_preorder = preorder[1:pos]
-            right_preorder = preorder[pos:]
 
-            pos = cls.find_pos(inorder, preorder[0])
-            left_inorder = inorder[:pos]
-            right_inorder = inorder[pos+1:]
+def generate_tree(preorder, inorder, postorder):
+    if len(preorder) != len(inorder) or len(inorder) != len(postorder):
+        raise RuntimeError("invalid sequence")
 
-            pos = cls.find_pos(postorder, preorder[1])
-            left_postorder = postorder[:pos+1]
-            right_postorder = postorder[pos+1:len(postorder)-1]
+    if len(preorder) == 0:
+        return None
 
-        if left_preorder:
-            cls.generate_tree(left_preorder,
-                left_inorder, left_postorder)
-        if right_preorder:
-            cls.generate_tree(right_preorder,
-                right_inorder, right_postorder)
-        return root
+    if len(preorder) == 1:
+        return NodeFactory.get_node(preorder[0])
+
+    if preorder[0] != postorder[-1]:
+        raise RuntimeError("invalid sequence")
+
+    # 先序序列的第一个节点和后序序列的最后一个节点是根节点
+    root = NodeFactory.get_node(preorder[0])
+
+    # 如果根节点是中序序列的第一个节点，说明根节点的左子树为空，先序序列的第二个节点是根节点的右孩子
+    if preorder[0] == inorder[0]:
+        root.left = None
+        root.right = NodeFactory.get_node(preorder[1])
+    # 如果根节点是中序序列的最后一个节点，说明根节点的右子树为空，先序序列的第二个节点是根节点的左孩子
+    elif preorder[0] == inorder[-1]:
+        root.left = NodeFactory.get_node(preorder[1])
+        root.right = None
+    # 否则，根节点的左右子树都不为空
+    else:
+        # 先序序列的第二个节点是根节点的左孩子
+        root.left = NodeFactory.get_node(preorder[1])
+        # 后序序列的倒数第二个节点是根节点的右孩子
+        root.right = NodeFactory.get_node(postorder[-2])
+
+    if root.left is None:
+        right_preorder = preorder[1:]
+        right_inorder = inorder[1:]
+        right_postorder = postorder[:-1]
+        generate_tree(right_preorder, right_inorder, right_postorder)
+    elif root.right is None:
+        left_preorder = preorder[1:]
+        left_inorder = inorder[:-1]
+        left_postorder = postorder[:-1]
+        generate_tree(left_preorder, left_inorder, left_postorder)
+    else:
+        pos = find_pos(preorder, root.right.keyword)
+        if pos == -1:
+            raise RuntimeError("invalid sequence")
+        left_preorder = preorder[1:pos]
+        right_preorder = preorder[pos:]
+
+        pos = find_pos(inorder, root.keyword)
+        if pos == -1:
+            raise RuntimeError("invalid sequence")
+        left_inorder = inorder[:pos]
+        right_inorder = inorder[pos+1:]
+
+        pos = find_pos(postorder, root.left.keyword)
+        if pos == -1:
+            raise RuntimeError("invalid sequence")
+        left_postorder = postorder[:pos+1]
+        right_postorder = postorder[pos+1:-1]
+
+        generate_tree(left_preorder, left_inorder, left_postorder)
+
+        generate_tree(right_preorder, right_inorder, right_postorder)
+
+    return root
+
+
+def test():
+    def preorder_traverse(p):
+        keywords = []
+        if p is None:
+            return keywords
+        keywords.append(p.keyword)
+        keywords.extend(preorder_traverse(p.left))
+        keywords.extend(preorder_traverse(p.right))
+        return keywords
+
+    def inorder_traverse(p):
+        keywords = []
+        if p is None:
+            return keywords
+        keywords.extend(inorder_traverse(p.left))
+        keywords.append(p.keyword)
+        keywords.extend(inorder_traverse(p.right))
+        return keywords
+
+    def postorder_traverse(p):
+        keywords = []
+        if p is None:
+            return keywords
+        keywords.extend(postorder_traverse(p.left))
+        keywords.extend(postorder_traverse(p.right))
+        keywords.append(p.keyword)
+        return keywords
+
+    nodes = [Node(ind) for ind in range(7)]
+    nodes[0].left = nodes[1]
+    nodes[0].right = nodes[2]
+    nodes[1].left = nodes[3]
+    nodes[1].right = nodes[4]
+    nodes[2].left = nodes[5]
+    nodes[3].right = nodes[6]
+
+    root = nodes[0]
+    del nodes
+
+    preorder = preorder_traverse(root)
+    inorder = inorder_traverse(root)
+    postorder = postorder_traverse(root)
+
+    generated_root = generate_tree(preorder, inorder, postorder)
+
+    assert preorder == preorder_traverse(generated_root)
+    assert inorder == inorder_traverse(generated_root)
+    assert postorder == postorder_traverse(generated_root)
+
 
 if __name__ == "__main__":
-    root = Node(100)
-    root.set_left(Node(200)).set_right(Node(300))
-    root.left.set_left(Node(400))
-    root.right.set_left(Node(500)).set_right(Node(600))
-    root.right.left.set_right(700)
-    root.right.right.set_left(800)
-
-    preorder = root.preorder_traverse()
-    inorder = root.inorder_traverse()
-    postorder = root.postorder_traverse()
-    print preorder, inorder, postorder
-    print Node.generate_tree(preorder, inorder, postorder).preorder_traverse() == preorder
-
+    test()
