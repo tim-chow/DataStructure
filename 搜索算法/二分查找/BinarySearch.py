7916f9750c1bdd5d0c85e343511f3ba15c35d6a9
TimChow <744475502@qq.com>
2020-07-20 14:35:18 +0800

    Update BinarySearch.py

--- 搜索算法/二分查找/BinarySearch.py
+++ 搜索算法/二分查找/BinarySearch.py
@@ -1,3 +1,20 @@
+def binary_search_recursive(array, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(array) - 1
+
+    if low > high:
+        return -1
+
+    mid = (low + high) / 2
+    if target == array[mid]:
+        return mid
+    if target < array[mid]:
+        return binary_search_recursive(array, target, low, mid - 1)
+    return binary_search_recursive(array, target, mid + 1, high)
+
+
 def binary_search(array, target):
     low = 0
     high = len(array) - 1
@@ -18,6 +35,7 @@ def test():
     array = list(range(20))
     for ind, element in enumerate(array):
         assert binary_search(array, element) == ind
+        assert binary_search_recursive(array, element) == ind
     assert binary_search(array, -1) == -1
 
 
