9d7bf5c2992041e375bd5dae27508ed17cbb902b
TimChow <744475502@qq.com>
2020-08-12 02:08:45 +0800

    Update dfs_tree.py

--- 图/dfs_tree.py
+++ 图/dfs_tree.py
@@ -1,7 +1,14 @@
-#coding: utf8
+# coding: utf8
 
-# 孩子兄弟表示法
-class Node:
+"""
+DFS 生成树
+"""
+
+
+class TreeNode(object):
+    """
+    树的孩子兄弟表示法
+    """
     def __init__(self, data):
         self._data = data
         self._first_child = None
@@ -25,48 +32,54 @@ class Node:
     def get_data(self):
         return self._data
 
+
 def get_adjacent(adjacent_list, index):
-    count = 0
+    real_index = 0
     for adjacent_index, weight in enumerate(adjacent_list):
         if weight is None:
             continue
-        if count == index:
+        if real_index == index:
             return adjacent_index
-        count = count + 1
+        real_index = real_index + 1
     return None
 
-def dfs_tree_recursive(start_index, start_node, 
-        visited, vertexes, edges):
-    visited[start_index] = True
-  
-    adjacent_list = edges[start_index]
-    index = 0
-    adjacent_index = get_adjacent(adjacent_list, index)
-    while adjacent_index is not None:
-        if visited[adjacent_index] == False:
-            node = Node(adjacent_index)
-            start_node.add_child(node)
-            dfs_tree_recursive(adjacent_index,
-                node, visited, vertexes, edges)
-        index = index + 1
+
+def dfs_tree_recursive(vertexes, edges):
+    visited = [False for _ in range(len(vertexes))]
+    root = TreeNode(vertexes[0])
+    visited[0] = True
+
+    def _dfs_tree_recursive(start_index, start_node):
+        adjacent_list = edges[start_index]
+        index = 0
         adjacent_index = get_adjacent(adjacent_list, index)
+        while adjacent_index is not None:
+            if not visited[adjacent_index]:
+                visited[adjacent_index] = True
+                node = TreeNode(vertexes[adjacent_index])
+                start_node.add_child(node)
+                _dfs_tree_recursive(adjacent_index, node)
+            index = index + 1
+            adjacent_index = get_adjacent(adjacent_list, index)
+
+    _dfs_tree_recursive(0, root)
+    return root
+
 
 def dfs_tree(vertexes, edges):
     root = None
     stack = [0]
     vertex_count = len(vertexes)
+    status = [0 for _ in range(vertex_count)]
+    visited = [False for _ in range(vertex_count)]
+    nodes = [None for _ in range(vertex_count)]
+    parent = [None for _ in range(vertex_count)]
 
-    # 记录每个顶点的第几个邻接点还没被访问
-    status = [0 for i in range(vertex_count)]
-    visited = [False for i in range(vertex_count)]
-    nodes = [None for i in range(vertex_count)]
-    parent = [None for i in range(vertex_count)]
-
-    while len(stack):
+    while stack:
         top = stack[-1]
-        if False == visited[top]:
+        if not visited[top]:
             visited[top] = True
-            node = Node(top)
+            node = TreeNode(vertexes[top])
             nodes[top] = node
             if parent[top] is None:
                 root = node
@@ -80,34 +93,51 @@ def dfs_tree(vertexes, edges):
         status[top] = status[top] + 1
         parent[adjacent_index] = top
         # 剪枝
-        if visited[adjacent_index] == True:
+        if visited[adjacent_index]:
             continue
         stack.append(adjacent_index)
     return root
 
-if __name__ == "__main__":
-    vertexes = range(4) 
-    edges = [[None] * 4, [None] * 4, [None] * 4, [None] * 4]
+
+def test():
+    vertexes = range(5)
+    edges = []
+    for ind in range(len(vertexes)):
+        edges.append([])
+        for _ in range(len(vertexes)):
+            edges[ind].append(None)
     edges[0][1] = 1
     edges[1][0] = 1
     edges[0][3] = 3
     edges[3][0] = 3
     edges[2][3] = 23
     edges[3][2] = 23
-    import pprint
-    pprint.pprint(edges)
+    edges[2][4] = 24
+    edges[4][2] = 24
+    edges[1][2] = 12
+    edges[2][1] = 12
 
-    root = Node(0)
-    visited = [False for _ in range(len(vertexes))]
-    dfs_tree_recursive(0, root, visited, vertexes, edges)
+    """
+    
+    0 --- 1 ----┐ 
+    |           |
+    └---- 3 --- 2 --- 4
+    """
 
-    root = dfs_tree(vertexes, edges)
+    root = dfs_tree_recursive(vertexes, edges)
 
-    print root.get_data()
-    first_child = root.get_first_child()
-    print first_child.get_data()
-    second_child = first_child.get_next_sibling()
-    print second_child.get_data()
-    first_grandson = second_child.get_first_child()
-    print first_grandson.get_data()
+    # root = dfs_tree(vertexes, edges)
 
+    assert root.get_data() == 0
+    first = root.get_first_child()
+    assert first.get_data() == 1
+    second = first.get_first_child()
+    assert second.get_data() == 2
+    third = second.get_first_child()
+    assert third.get_data() == 3
+    forth = third.get_next_sibling()
+    assert forth.get_data() == 4
+
+
+if __name__ == "__main__":
+    test()
