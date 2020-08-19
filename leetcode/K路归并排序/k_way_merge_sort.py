b05a051bec66b9d1d604d53f55268d22db25a76b
TimChow <744475502@qq.com>
2020-08-19 11:49:34 +0800

    Update k_way_merge_sort.py

--- leetcode/K路归并排序/k_way_merge_sort.py
+++ leetcode/K路归并排序/k_way_merge_sort.py
@@ -1,14 +1,16 @@
-class MinHeap:
+# coding: utf8
+
+
+class MinHeap(object):
     @staticmethod
     def adjust(heap, start=None, end=None):
-        get_parent = lambda ind: (ind - 1) / 2
+        if start is None:
+            start = 0
+        if end is None:
+            end = len(heap) - 1
+
         get_left_child = lambda ind: 2 * ind + 1
         get_right_child = lambda ind: 2 * ind + 2
-
-        if start == None:
-            start = 0
-        if end == None:
-            end = len(heap) - 1
 
         index = start
         while index >= 0:
@@ -23,7 +25,7 @@ class MinHeap:
                 break
 
             if heap[index] <= heap[left_child_index] and \
-                heap[index] <= heap[right_child_index]:
+                    heap[index] <= heap[right_child_index]:
                 break
             if heap[right_child_index] > heap[left_child_index]:
                 heap[index], heap[left_child_index] = \
@@ -39,58 +41,75 @@ class MinHeap:
         for ind in range(len(heap)/2, -1, -1):
             cls.adjust(heap, ind)
 
-class Iterator:
-    def __init__(self, array):
-        self._array = array
-        self._cursor = 0
 
-    def has_next(self):
-        return self._cursor < len(self._array)
+class Node(object):
+    def __init__(self, cursor, array):
+        self.cursor = cursor
+        self.array = array
 
-    def next(self):
-        result = self._array[self._cursor]
-        self._cursor = self._cursor + 1
-        return result
-
-    def peek(self):
-        return self._array[self._cursor]
-
-    def __cmp__(self, another):
-        self_peek = self.peek()
-        another_peek = another.peek()
-        if self_peek < another_peek:
-            return -1
-        if self_peek > another_peek:
+    def __cmp__(self, other):
+        diff = self.array[self.cursor] - other.array[other.cursor]
+        if diff > 0:
             return 1
-        return 0
+        elif diff == 0:
+            return 0
+        else:
+            return -1
+
+    def get_next_node(self):
+        if self.cursor >= len(self.array) - 1:
+            return None
+        return Node(self.cursor + 1, self.array)
+
+    def get_element(self):
+        return self.array[self.cursor]
+
 
 def k_way_merge_sort(arrays):
-    result = [None] * sum([len(lst) for lst in arrays])
+    result = [None] * sum([len(array) for array in arrays])
+    # 取每个数组的第一个元素，建立一个最小堆
+    heap = [Node(0, array) for array in arrays]
+    MinHeap.heapify(heap)
+
     index = 0
-
     k = len(arrays) - 1
-    iterators = map(Iterator, arrays)
-    MinHeap.heapify(iterators)
 
-    while k >= 0:
-        top = iterators[0]
-        result[index] = top.next()
+    while True:
+        # 弹出堆顶元素
+        node = heap[0]
+        result[index] = node.get_element()
         index = index + 1
-        if top.has_next():
-            MinHeap.adjust(iterators, 0, k)
+
+        # 如果堆顶元素所在的数组仍有剩余元素，那么从其中取下一个元素，然后放到堆顶
+        next_node = node.get_next_node()
+        if next_node is not None:
+            heap[0] = next_node
+            # 调整堆
+            MinHeap.adjust(heap, 0, k)
             continue
-        iterators[0] = iterators[k]
+
+        # 如果堆空，则退出
+        if k == 0:
+            break
+
+        # 否则将堆的最后一个元素弹出，然后放到堆顶
+        heap[0] = heap[k]
+        # 调整堆
         k = k - 1
-        MinHeap.adjust(iterators, 0, k)
+        MinHeap.adjust(heap, 0, k)
 
     return result
 
-if __name__ == "__main__":
-    arrays = [
-        [1, 5, 9, 13, 15, 17],
-        [4, 8, 12, 14, 16, 18],
-        [3, 7, 11, 30],
-        [2, 6, 10],
-    ]
-    print k_way_merge_sort(arrays)
 
+def test():
+    arrays = []
+    for ind in range(1, 10):
+        array = range(100 * ind, 100 * ind + 100)
+        arrays.append(array)
+    result = k_way_merge_sort(arrays)
+    for ind in range(0, len(result) - 1):
+        assert result[ind] <= result[ind + 1]
+
+
+if __name__ == "__main__":
+    test()
