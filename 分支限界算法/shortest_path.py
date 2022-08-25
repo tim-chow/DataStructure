38abab46d0f6d633751d28f30b29d2e3fb11c2d9
TimChow <744475502@qq.com>
2022-08-26 00:25:15 +0800

    Update shortest_path.py

--- 分支限界算法/shortest_path.py
+++ 分支限界算法/shortest_path.py
@@ -1,72 +1,13 @@
-# coding: utf8
+from queue import PriorityQueue
+from typing import List, Dict, Tuple
+
+GraphType = Tuple[List[str], List[List[int]]]
 
 
-# 最小堆
-class MinHeap(object):
-    get_parent = lambda self, ind: (ind - 1) / 2
-    get_left_child = lambda self, ind: 2 * ind + 1
-    get_right_child = lambda self, ind: 2 * ind + 2
-
-    def __init__(self):
-        self._array = []
-
-    def _adjust(self, array, start=None, end=None):
-        if start is None:
-            start = 0
-        if end is None:
-            end = len(array) - 1
-
-        while self.get_left_child(start) <= end:
-            left_child_index = self.get_left_child(start)
-            right_child_index = self.get_right_child(start)
-
-            if right_child_index > end:
-                if array[start] > array[left_child_index]:
-                    array[start], array[left_child_index] = \
-                        array[left_child_index], array[start]
-                break
-
-            if array[start] <= array[left_child_index] and \
-                    array[start] <= array[right_child_index]:
-                break
-
-            if array[right_child_index] > array[left_child_index]:
-                array[start], array[left_child_index] = \
-                    array[left_child_index], array[start]
-                start = left_child_index
-            else:
-                array[start], array[right_child_index] = \
-                    array[right_child_index], array[start]
-                start = right_child_index
-
-    def put(self, priority, element):
-        self._array.append((priority, element))
-        index = len(self._array) - 1
-        parent = self.get_parent(index)
-
-        while parent >= 0:
-            self._adjust(self._array, parent)
-            parent = self.get_parent(parent)
-
-    def get(self):
-        if not self._array:
-            raise RuntimeError("queue is empty")
-        result = self._array[0]
-        last = self._array.pop(-1)
-        if self._array:
-            self._array[0] = last
-            self._adjust(self._array, 0)
-        return result
-
-    def empty(self):
-        return not len(self._array)
-
-
-# 图的数组/矩阵表示法
-def generate_direct_graph_example():
-    vertexes = ['s', 'a', 'b', 'c', 'd', 'e',
-        'f', 'g', 'h', 'i', 't']
-    arches = [
+def generate_direct_graph_example() -> GraphType:
+    vertexes: List[str] = ['s', 'a', 'b', 'c', 'd', 'e',
+                           'f', 'g', 'h', 'i', 't']
+    arches: List[List[int]] = [
         [0, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 3, 0, 7, 2, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 9, 2, 0, 0, 0, 0],
@@ -82,32 +23,28 @@ def generate_direct_graph_example():
     return vertexes, arches
 
 
-def shortest_path(graph, start):
-    queue = MinHeap()
-    queue.put(0, start)
-    result = {}
-
+def shortest_path(graph: GraphType, start: int) -> Dict[int, int]:
+    queue: PriorityQueue = PriorityQueue()
+    queue.put((0, start))
+    result: Dict[int, int] = {}
     while not queue.empty():
-        current_weight, vertex_index = queue.get()
-        for ind, weight in enumerate(graph[1][vertex_index]):
+        # 第一个活节点是当前扩展节点
+        current_weight, index = queue.get()  # type: int, int
+        # 列出当前扩展节点的所有孩子节点
+        for index, weight in enumerate(graph[1][index]):  # type: int, int
             if not weight:
                 continue
-            total_weight = current_weight + weight
-            if ind not in result or total_weight < result[ind]:
-                result[ind] = total_weight
-                queue.put(total_weight, ind)
+            # 剪去无法到达最优解的孩子节点
+            if index in result and result[index] <= weight + current_weight:
+                continue
+            # 将其余孩子节点添加到活结点列表，并使用限界函数进行排序
+            result[index] = weight + current_weight
+            queue.put((result[index], index))
     return result
 
 
 if __name__ == "__main__":
-    import pprint
-
-    graph = generate_direct_graph_example()
-    pprint.pprint(graph)
-
-    result = shortest_path(graph, 0)
-    print(
-        "\n".join(["从%s到%s的距离是：%-3d" %
-            (graph[0][0], graph[0][k], v)
-            for k, v in result.items()])
-    )
+    graph: GraphType = generate_direct_graph_example()
+    result: Dict[int, int] = shortest_path(graph, 0)
+    for idx, weight in result.items():
+        print(f"s -> {graph[0][idx]}: {weight}")
