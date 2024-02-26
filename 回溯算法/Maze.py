0f97a9b99afb8370b8a34d1b17a034c27db1965a
tim-chow <744475502@qq.com>
2024-02-26 16:26:26 +0800

    修改迷宫问题的 Python 实现

--- 回溯算法/Maze.py
+++ 回溯算法/Maze.py
@@ -1,71 +1,72 @@
-class Node(object):
-    def __init__(self, row, column, is_wall):
-        self._row = row
-        self._column = column
-        self._is_wall = bool(is_wall)
-        self._next_nodes = []
-        self._cursor = 0
-
-    @property
-    def row(self):
-        return self._row
-
-    @property
-    def column(self):
-        return self._column
-
-    @property
-    def is_wall(self):
-        return self._is_wall
-
-    def add_next_node(self, next_node):
-        if not isinstance(next_node, Node):
-            raise RuntimeError("Node expected")
-        self._next_nodes.append(next_node)
-
-    def get_next_node(self):
-        if self._cursor >= len(self._next_nodes):
-            return
-        node = self._next_nodes[self._cursor]
-        self._cursor = self._cursor + 1
-        return node
-
-    def __eq__(self, o):
-        if not isinstance(o, Node):
-            return False
-        return o.row == self.row and o.column == self.column
-
-    def __str__(self):
-        return "%s{row=%d, column=%d, is_wall=%s}" % (
-            self.__class__.__name__,
-            self.row,
-            self.column,
-            self._is_wall)
-    __repr__ = __str__
+import copy
+from typing import List, Tuple
 
 
-def search(start, end):
-    stack = [start]
+def search(maze: List[List[int]], start: Tuple[int, int], end: Tuple[int, int]) -> List[List[Tuple[int, int]]]:
+    result: List[List[Tuple[int, int]]] = []
 
+    # 1 代表上面的格子待搜索；
+    # 2 代表右面的格子待搜索
+    # 3 代表下面的格子待搜索
+    # 4 代表左面的格子待搜索
+    # 5 代表上下左右都已搜索
+    status: List[List[int]] = []
+    for row in range(len(maze)):  # type: int
+        status.append([1] * len(maze[row]))
+
+    stack: List[Tuple[int, int]] = [start]
     while stack:
-        current_expand_node = stack[-1]
-        next_node = current_expand_node.get_next_node()
-        if next_node is None:
-            stack.pop(-1)
+        current_expand_node: Tuple[int, int] = stack[-1]
+        x, y = current_expand_node  # type: int, int
+        match status[x][y]:
+            case 1:
+                status[x][y] = 2
+                if x == 0:
+                    continue
+                next_node: Tuple[int, int] = (x - 1, y)
+            case 2:
+                status[x][y] = 3
+                if y == len(maze[x]) - 1:
+                    continue
+                next_node: Tuple[int, int] = (x, y + 1)
+            case 3:
+                status[x][y] = 4
+                if x == len(maze) - 1:
+                    continue
+                next_node: Tuple[int, int] = (x + 1, y)
+            case 4:
+                status[x][y] = 5
+                if y == 0:
+                    continue
+                next_node: Tuple[int, int] = (x, y - 1)
+            case _:
+                # 无法继续向纵深方向前进
+                # 恢复状态
+                status[x][y] = 1
+                # 将死节点从栈中弹出
+                stack.pop(-1)
+                continue
+        x, y = next_node  # type: int, int
+        # 新节点是墙
+        if maze[x][y] == 0:
             continue
-
-        if next_node.is_wall or next_node in stack:
+        # 已经在栈中
+        if next_node in stack:
             continue
-
+        # 到达终点
         if next_node == end:
-            print(stack + [next_node])
+            path: List[Tuple[int, int]] = copy.copy(stack)
+            path.append(next_node)
+            result.append(path)
             continue
-
+        # 使其成为活结点
         stack.append(next_node)
 
+    return result
 
-def main():
-    # 0 means wall, 1 means road
+
+def main() -> None:
+    # 0 代表墙；1 代表路
     maze = [
         [0, 0, 0, 0, 0],
         [1, 1, 1, 1, 0],
@@ -73,31 +74,10 @@ def main():
         [1, 1, 0, 1, 0],
         [0, 1, 1, 1, 0],
     ]
-
-    all_nodes = []
-    for row in range(len(maze)):
-        nodes = []
-        for column in range(len(maze[row])):
-            nodes.append(Node(row, column, not maze[row][column]))
-        all_nodes.append(nodes)
-
-    for row in range(len(all_nodes)):
-        for column in range(len(all_nodes[row])):
-            if row > 0:
-                all_nodes[row][column].\
-                    add_next_node(all_nodes[row-1][column])
-            if row < len(all_nodes) - 1:
-                all_nodes[row][column].\
-                    add_next_node(all_nodes[row+1][column])
-            if column > 0:
-                all_nodes[row][column].\
-                    add_next_node(all_nodes[row][column-1])
-            if column < len(all_nodes[row]) - 1:
-                all_nodes[row][column].\
-                    add_next_node(all_nodes[row][column+1])
-
-    search(all_nodes[1][1], all_nodes[3][1])
+    for path in search(maze, (1, 0), (3, 0)):
+        print(path)
 
 
 if __name__ == "__main__":
     main()
+
