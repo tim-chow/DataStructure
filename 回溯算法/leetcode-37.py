07919ef2450b534a7455b1dc658a4066e1d7e497
tim-chow <744475502@qq.com>
2024-02-29 14:21:17 +0800

    修改 leetcode 37 - 解数独的 Python 实现

--- 回溯算法/leetcode-37.py
+++ 回溯算法/leetcode-37.py
@@ -8,7 +8,11 @@ class Solution:
         return x // 9, x % 9
 
     def get_constraints(self, matrix: List[List[int]], x: int, y: int) -> Set[int]:  # noqa
-        # 3x3
+        """
+        获取同一 3x3 格子，同一行，同一列的所有数字
+        """
+
+        # 获取 3x3 格子的中间
         def get_center(a: int) -> int:
             if a in {0, 1, 2}:
                 return 1
@@ -19,7 +23,8 @@ class Solution:
 
         center_x: int = get_center(x)
         center_y: int = get_center(y)
-        s: Set = set()
+        s: Set[int] = set()
+        # 获取同一 3x3 格子中的数字
         for coord in [
             (center_x - 1, center_y - 1), (center_x - 1, center_y), (center_x - 1, center_y + 1),
             (center_x, center_y - 1), (center_x, center_y), (center_x, center_y + 1),
@@ -31,15 +36,15 @@ class Solution:
             if element:
                 s.add(element)
 
-        # row
-        for ind in range(0, 9):
+        # 获取同一行的数字
+        for ind in range(0, 9):  # type: int
             if ind != y:
                 element: int = matrix[x][ind]
                 if element:
                     s.add(element)
 
-        # column
-        for ind in range(0, 9):
+        # 获取同一列的数字
+        for ind in range(0, 9):  # type: int
             if ind != x:
                 element: int = matrix[ind][y]
                 if element:
@@ -48,10 +53,11 @@ class Solution:
         return s
 
     def solveSudoku(self, board: List[List[str]]) -> None:  # noqa
+        # 初始状态
         status: List[List[int]] = []
-        for row in board:
+        for row in board:  # type: List[str]
             status.append([])
-            for column in row:
+            for column in row:  # type: str
                 if column == ".":
                     status[-1].append(0)
                 else:
@@ -59,13 +65,13 @@ class Solution:
 
         top: int = 0
         while top < 81:
-            x, y = self.to_matrix(top)
+            x, y = self.to_matrix(top)  # type: int, int
             # 已给定数字直接填入
             if board[x][y] != ".":
                 top += 1
                 continue
             constraints: Set[int] = self.get_constraints(status, x, y)
-            for new_value in range(status[x][y] + 1, 10):
+            for new_value in range(status[x][y] + 1, 10):  # type: int
                 if new_value in constraints:
                     continue
                 status[x][y] = new_value
@@ -75,7 +81,7 @@ class Solution:
                 # 回溯到最近的 “.”
                 top -= 1
                 while top >= 0:
-                    temp_x, temp_y = self.to_matrix(top)
+                    temp_x, temp_y = self.to_matrix(top)  # type: int, int
                     if board[temp_x][temp_y] != ".":
                         top -= 1
                         continue
@@ -85,8 +91,8 @@ class Solution:
                 # 重置状态
                 status[x][y] = 0
         else:
-            for i in range(len(board)):
-                for j in range(len(board[i])):
+            for i in range(len(board)):  # type: int
+                for j in range(len(board[i])):  # type: int
                     if board[i][j] == ".":
                         board[i][j] = str(status[i][j])
             return
