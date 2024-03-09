14fde821389f564bc08b34025995163f7da9f7ff
tim-chow <744475502@qq.com>
2024-03-09 17:22:42 +0800

    新增非递归归并排序的 Go 实现

--- 排序算法/归并排序/merge_sort.go
+++ 排序算法/归并排序/merge_sort.go
@@ -35,22 +35,44 @@ func merge(a []int, start, mid, end int) {
 	}
 }
 
-func mergeSort(a []int, start, end int) {
+func mergeSortRecursive(a []int, start, end int) {
 	if end <= start {
 		return
 	}
 	mid := (start + end) / 2
-	mergeSort(a, start, mid)
-	mergeSort(a, mid+1, end)
+	mergeSortRecursive(a, start, mid)
+	mergeSortRecursive(a, mid+1, end)
 	merge(a, start, mid, end)
 }
 
+func MergeSortRecursive(a []int) {
+	mergeSortRecursive(a, 0, len(a)-1)
+}
+
 func MergeSort(a []int) {
-	mergeSort(a, 0, len(a)-1)
+	step := 1
+	for step <= len(a) {
+		step *= 2
+		for start := 0; start < len(a); start += step {
+			end := start + step - 1
+			mid := (start + end) / 2
+			if end >= len(a) {
+				end = len(a) - 1
+				mid = start + step/2 - 1
+				if mid > end {
+					mid = end
+				}
+			}
+			merge(a, start, mid, end)
+		}
+	}
 }
 
 func main() {
 	a := []int{3, 2, 9, -1, 88, 66, 1, 33, -3, 33, 88, 2, 9, 3}
 	MergeSort(a)
+	fmt.Println(a)
+	a = []int{3, 2, 9, -1, 88, 66, 1, 33, -3, 33, 88, 2, 9, 3}
+	MergeSortRecursive(a)
 	fmt.Println(a)
 }
